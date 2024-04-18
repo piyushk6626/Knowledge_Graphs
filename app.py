085c8ab46cbdaf083fc88d95c5a8d69bad001c86
piyushk6626 <143712946+piyushk6626@users.noreply.github.com>
2024-04-18 22:27:08 +0530

    uploaded files

---
+++ app.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+
+from langchain_community.graphs import Neo4jGraph
+
+# Warning control
+import warnings
+warnings.filterwarnings("ignore")
+
+# Load from environment
+load_dotenv('.env', override=True)
+NEO4J_URI = os.getenv('NEO4J_URI')
+NEO4J_USERNAME = os.getenv('NEO4J_USERNAME')
+NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
+NEO4J_DATABASE = os.getenv('NEO4J_DATABASE')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# Note the code below is unique to this course environment, and not a 
+# standard part of Neo4j's integration with OpenAI. Remove if running 
+# in your own environment.
+OPENAI_ENDPOINT = os.getenv('OPENAI_BASE_URL') + '/embeddings'
---
+++ main.py
@@ -0,0 +1,94 @@
+import streamlit as st
+from streamlit_chat import message
+from timeit import default_timer as timer
+
+from langchain_community.graphs import Neo4jGraph
+from langchain.chains import GraphCypherQAChain
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+# OpenAI API configuration
+llm = ChatOpenAI(model="gpt-3.5-turbo",temperature=0)
+# Load from environment
+
+NEO4J_URL = os.getenv('NEO4J_URI')
+NEO4J_USERNAME = os.getenv('NEO4J_USERNAME')
+NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
+NEO4J_DATABASE = os.getenv('NEO4J_DATABASE')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+# Cypher generation prompt
+cypher_generation_template = """
+You are an expert Neo4j Cypher translator who converts English to Cypher based on the Neo4j Schema provided, following the instructions below:
+1. Generate Cypher query compatible ONLY for Neo4j Version 5
+2. Do not use EXISTS, SIZE, HAVING keywords in the cypher. Use alias when using the WITH keyword
+3. Use only Nodes and relationships mentioned in the schema
+4. Always do a case-insensitive and fuzzy search for any properties related search. Eg: to search for a Client, use `toLower(client.id) contains 'neo4j'`. To search for Slack Messages, use 'toLower(SlackMessage.text) contains 'neo4j'`. To search for a project, use `toLower(project.summary) contains 'logistics platform' OR toLower(project.name) contains 'logistics platform'`.)
+5. Never use relationships that are not mentioned in the given schema
+6. When asked about projects, Match the properties using case-insensitive matching and the OR-operator, E.g, to find a logistics platform -project, use `toLower(project.summary) contains 'logistics platform' OR toLower(project.name) contains 'logistics platform'`.
+
+schema: {schema}
+
+Examples:
+Question: Which client's projects use most of our people?
+Answer: ```MATCH (c:CLIENT)<-[:HAS_CLIENT]-(p:Project)-[:HAS_PEOPLE]->(person:Person)
+RETURN c.name AS Client, COUNT(DISTINCT person) AS NumberOfPeople
+ORDER BY NumberOfPeople DESC```
+Question: Which person uses the largest number of different technologies?
+Answer: ```MATCH (person:Person)-[:USES_TECH]->(tech:Technology)
+RETURN person.name AS PersonName, COUNT(DISTINCT tech) AS NumberOfTechnologies
+ORDER BY NumberOfTechnologies DESC```
+
+Question: {question}
+"""
+
+cypher_prompt = PromptTemplate(
+    template = cypher_generation_template,
+    input_variables = ["schema", "question"]
+)
+
+CYPHER_QA_TEMPLATE = """You are an assistant that helps to form nice and human understandable answers.
+The information part contains the provided information that you must use to construct an answer.
+The provided information is authoritative, you must never doubt it or try to use your internal knowledge to correct it.
+Make the answer sound as a response to the question. Do not mention that you based the result on the given information.
+If the provided information is empty, say that you don't know the answer.
+Final answer should be easily readable and structured.
+Information:
+{context}
+
+Question: {question}
+Helpful Answer:"""
+
+qa_prompt = PromptTemplate(
+    input_variables=["context", "question"], template=CYPHER_QA_TEMPLATE
+)
+
+def query_graph(user_input):
+    graph = Neo4jGraph(url="neo4j+s://013d8f5e.databases.neo4j.io", username=NEO4J_USERNAME, password=NEO4J_PASSWORD, database="neo4j")   
+    chain = GraphCypherQAChain.from_llm(
+        llm=llm,
+        graph=graph,
+        verbose=True,
+        return_intermediate_steps=True,
+        cypher_prompt=cypher_prompt,
+        qa_prompt=qa_prompt
+        )
+    result = chain(user_input)
+    return result
+
+
+result = query_graph("how to buy insurance")
+
+intermediate_steps = result["intermediate_steps"]
+cypher_query = intermediate_steps[0]["query"]
+database_results = intermediate_steps[1]["context"]
+
+answer = result["result"]
+
+print(result)
+    
